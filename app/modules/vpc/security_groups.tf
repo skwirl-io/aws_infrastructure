9ce037b8d5e543abb2ecb07dd998b0bdcee60f08
wivarn <wivarn@gmail.com>
2021-05-31 05:28:05 -0700

    add some params

--- app/modules/vpc/security_groups.tf
+++ app/modules/vpc/security_groups.tf
@@ -2,22 +2,26 @@ resource "aws_security_group" "lambda_sg" {
   name        = "default-lambda-sg"
   description = "default security group for lambdas"
   vpc_id      = aws_vpc.main.id
+}
 
-  egress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "lambda_sg_allow_http" {
+  security_group_id        = aws_security_group.lambda_sg.id
+  type                     = "egress"
+  description              = "Allow HTTP"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  cidr_blocks              = ["0.0.0.0/0"]
+}
 
-  egress {
-    description = "Allow HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "lambda_sg_allow_https" {
+  security_group_id        = aws_security_group.lambda_sg.id
+  type                     = "egress"
+  description              = "Allow HTTPS"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  cidr_blocks              = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "lambda_sg_to_rds_sg" {
--- app/stacks/base/main.tf
+++ app/stacks/base/main.tf
@@ -1,3 +1,7 @@
+locals {
+  ssm_parameter_prefix = "/market_back/staging/"
+}
+
 module "vpc" {
   source = "../../modules/vpc"
 }
@@ -7,5 +11,27 @@ module "rds_instance" {
 
   db_subnet_group_name  = module.vpc.db_subnet_group_name
   rds_security_group_id = module.vpc.rds_security_group_id
-  ssm_parameter_prefix  = "/market_back/staging/"
+  ssm_parameter_prefix  = local.ssm_parameter_prefix
+}
+
+# TODO: rotate and ofuscate
+resource "aws_ssm_parameter" "hmac_secret" {
+  name        = "${local.ssm_parameter_prefix}HMAC_SECRET"
+  description = "hmac secret for Rodauth"
+  type        = "SecureString"
+  value       = "cbaeecb1b20575852bfb428664040d9ecfe4a6c73d2cece79bb15769449e76093841931d48776e4436d7d218a1a1e5fc6a73ecb82d044e7400b21a083537bfe3"
+}
+
+resource "aws_ssm_parameter" "password_pepper" {
+  name        = "${local.ssm_parameter_prefix}PASSWORD_PEPPER"
+  description = "password pepper for Rodauth"
+  type        = "SecureString"
+  value       = "0b650267ae61f2be1aa36c5a5ce1c3b1b61709886a51ee53016fb807d0bd935673f8b0aa63267772dab573acb6fbe30c7306081bdf14b7b01d4ec9ef4392fce6"
+}
+
+resource "aws_ssm_parameter" "jwt_secret" {
+  name        = "${local.ssm_parameter_prefix}JWT_SECRET"
+  description = "jwt secret for Rodauth"
+  type        = "SecureString"
+  value       = "657e57e5784301eeab3dcbfef181d6b86d5c97eb3dd2770ee89f1b656248311c068e45a46e796d254be3cbacfaa96da60426696c99a68d4d5a3978a2b6d4b2d3"
 }
