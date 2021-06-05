5fd5dceed0ed56b5eb492a0b7b1bc970ebac7d9c
wivarn <wivarn@gmail.com>
2021-06-04 21:28:43 -0700

    fix typo

--- app/modules/route53/ses.tf
+++ app/modules/route53/ses.tf
@@ -3,7 +3,7 @@ resource "aws_ses_domain_identity" "email_domain" {
 }
 
 resource "aws_route53_record" "ses_verification_record" {
-  zone_id = aws_route53_zone.zone.zone_id
+  zone_id = data.aws_route53_zone.zone.zone_id
   name    = "_amazonses.${aws_ses_domain_identity.email_domain.id}"
   type    = "TXT"
   ttl     = "600"
--- app/stacks/base/variables.tf
+++ app/stacks/base/variables.tf
@@ -1,3 +1,7 @@
 variable "ssm_parameter_prefix" {
   description = "The prefix to give to all SSM params in this stack"
 }
+
+variable "domain" {
+  description = "the base domain"
+}
