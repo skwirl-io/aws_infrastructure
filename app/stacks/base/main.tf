df1e726edff42e5177df94a5e88774f98cf9b463
wivarn <wivarn@gmail.com>
2021-06-25 18:22:06 -0700

    tf fmt

--- app/stacks/base/main.tf
+++ app/stacks/base/main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
   source = "../../modules/vpc"
 
-  ssm_parameter_prefix  = var.ssm_parameter_prefix
+  ssm_parameter_prefix = var.ssm_parameter_prefix
 }
 
 module "rds_instance" {
   source = "../../modules/rds_instance"
-  count = var.environment == "production" ? 0 : 1
+  count  = var.environment == "production" ? 0 : 1
 
   db_subnet_group_name  = module.vpc.db_subnet_group_name
   rds_security_group_id = module.vpc.rds_security_group_id
@@ -16,13 +16,13 @@ module "rds_instance" {
 module "route53" {
   source = "../../modules/route53"
 
-  domain = var.domain
-  ssm_parameter_prefix  = var.ssm_parameter_prefix
+  domain               = var.domain
+  ssm_parameter_prefix = var.ssm_parameter_prefix
 }
 
 module "backend_parameters" {
   source = "../../modules/backend_parameters"
 
-  ssm_parameter_prefix  = var.ssm_parameter_prefix
-  domain = var.domain
+  ssm_parameter_prefix = var.ssm_parameter_prefix
+  domain               = var.domain
 }
