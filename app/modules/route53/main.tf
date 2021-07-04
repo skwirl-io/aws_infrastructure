2400927cb86eb65c8994fba0d4b513113907ec68
wivarn <wivarn@gmail.com>
2021-07-04 00:18:49 -0700

    add blog dns record

--- app/modules/route53/main.tf
+++ app/modules/route53/main.tf
@@ -17,3 +17,11 @@ resource "aws_route53_record" "vercel_a" {
   ttl     = "300"
   records = ["76.76.21.21"]
 }
+
+resource "aws_route53_record" "vercel_blog_cname" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "blog"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["cname.vercel-dns.com"]
+}
