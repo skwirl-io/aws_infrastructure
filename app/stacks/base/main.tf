5ebd4653264c2fc7a1fec8a6d3a788fc26b01f54
wivarn <wivarn@gmail.com>
2021-08-03 08:32:48 -0700

    turn more things off in prod

--- app/stacks/base/main.tf
+++ app/stacks/base/main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source = "../../modules/vpc"
+  count  = var.environment == "production" ? 0 : 1
 
   ssm_parameter_prefix = var.ssm_parameter_prefix
 }
@@ -8,13 +9,14 @@ module "rds_instance" {
   source = "../../modules/rds_instance"
   count  = var.environment == "production" ? 0 : 1
 
-  db_subnet_group_name  = module.vpc.db_subnet_group_name
-  rds_security_group_id = module.vpc.rds_security_group_id
+  db_subnet_group_name  = var.environment == "production" ? 0 : module.vpc.db_subnet_group_name
+  rds_security_group_id = var.environment == "production" ? 0 : module.vpc.rds_security_group_id
   ssm_parameter_prefix  = var.ssm_parameter_prefix
 }
 
 module "s3" {
   source = "../../modules/s3"
+  count  = var.environment == "production" ? 0 : 1
 
   domain               = var.domain
   public_assets_bucket = var.public_assets_bucket
